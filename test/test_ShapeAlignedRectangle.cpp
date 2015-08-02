6aaca1da1c13c1caa1e61752db997814c3413e08
Christoph Rist <rist.christoph@gmail.com>
2015-08-02 23:09:36 +0200

    Linking tests against xkcd_binpack, basic test

--- test/test_ShapeAlignedRectangle.cpp
+++ test/test_ShapeAlignedRectangle.cpp
@@ -4,7 +4,20 @@
 
 #include <gtest/gtest.h>
 
+#include <xkcd_binpack/ShapeAlignedRectangle.h>
+
+using namespace xkcd_binpack;
+
 TEST(test_ShapeAlignedRectangle, collision_simple)
 {
-  ASSERT_EQ(true, true);
+  ShapeAlignedRectangle r1;
+  r1.setPosition(Pixel(0,0));
+  r1.setDimensions(1,1);
+
+  ShapeAlignedRectangle r2;
+  r2.setPosition(Pixel(0,0));
+  r2.setDimensions(1,1);
+
+  ASSERT_EQ(r1.checkCollision(r1), true);
+  ASSERT_EQ(r1.checkCollision(r2), true);
 }
