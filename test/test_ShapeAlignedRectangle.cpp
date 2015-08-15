789d38246d24752e523bfb7c9e303f1e5678cb73
Christoph Rist <rist.christoph@gmail.com>
2015-08-15 22:11:41 +0200

    [test] Pixel collision test cases

--- test/test_ShapeAlignedRectangle.cpp
+++ test/test_ShapeAlignedRectangle.cpp
@@ -8,7 +8,34 @@
 
 using namespace xkcd_binpack;
 
-TEST(test_ShapeAlignedRectangle, collision_simple)
+TEST(test_ShapeAlignedRectangle, collision_pixel)
+{
+  ShapeAlignedRectangle r1;
+
+  ASSERT_EQ(r1.checkCollision(Pixel(0,0)), true);
+
+  ASSERT_EQ(r1.checkCollision(Pixel(1,0)), false);
+  ASSERT_EQ(r1.checkCollision(Pixel(1,1)), false);
+  ASSERT_EQ(r1.checkCollision(Pixel(0,1)), false);
+  ASSERT_EQ(r1.checkCollision(Pixel(-1,1)), false);
+  ASSERT_EQ(r1.checkCollision(Pixel(-1,0)), false);
+  ASSERT_EQ(r1.checkCollision(Pixel(-1,-1)), false);
+  ASSERT_EQ(r1.checkCollision(Pixel(0,-1)), false);
+  ASSERT_EQ(r1.checkCollision(Pixel(1,-1)), false);
+
+  r1.setPosition(Pixel(10, -5));
+  r1.setDimensions(4,2);
+
+  ASSERT_EQ(r1.checkCollision(Pixel(11,-6)), false);
+  ASSERT_EQ(r1.checkCollision(Pixel(11,-5)), true);
+  ASSERT_EQ(r1.checkCollision(Pixel(11,-4)), true);
+  ASSERT_EQ(r1.checkCollision(Pixel(11,-3)), false);
+
+  ASSERT_EQ(r1.checkCollision(Pixel(14,-5)), false);
+  ASSERT_EQ(r1.checkCollision(Pixel(14,-4)), false);
+}
+
+TEST(test_ShapeAlignedRectangle, collision_other)
 {
   ShapeAlignedRectangle r1;
   r1.setPosition(Pixel(0,0));
